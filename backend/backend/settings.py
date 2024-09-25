69349c81d0badfa1a8ebbe93be263fd54a48866e
TeaToe17 <titobiloluwa@gmail.com>
2024-09-25 13:09:02 +0100

    correct origin

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -177,7 +177,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOW_ALL_ORIGINS = False
 CORS_ALLOWED_ORIGINS = [
-    "https://your-frontend.vercel.app", 
+    "https://django-reactfrontend.vercel.app", 
 ]
 
 CORS_ALLOW_CREDENTIALS = True
